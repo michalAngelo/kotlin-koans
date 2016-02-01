bcc306ed1a4a68354d65c1adac2d65b31ee78abd
Michał Piekarski <michal.piekarski@byoutline.com>
2016-02-01 12:19:31 +0100

    default and named params

--- src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
+++ src/i_introduction/_3_Default_Arguments/DefaultAndNamedParams.kt
@@ -1,6 +1,7 @@
 package i_introduction._3_Default_Arguments
 
-import util.*
+import util.TODO
+import util.doc2
 
 fun todoTask3(): Nothing = TODO(
     """
@@ -13,12 +14,13 @@ fun todoTask3(): Nothing = TODO(
     documentation = doc2(),
     references = { name: String -> JavaCode3().foo(name); foo(name) })
 
-fun foo(name: String): String = todoTask3()
+fun foo(name: String,number: Int = 42,toUpperCase: Boolean = false) =
+    (if(toUpperCase) name.toUpperCase() else name)+number
 
 fun task3(): String {
-    todoTask3()
-//    return (foo("a") +
-//            foo("b", number = 1) +
-//            foo("c", toUpperCase = true) +
-//            foo(name = "d", number = 2, toUpperCase = true))
+    //todoTask3()
+    return (foo("a") +
+            foo("b", number = 1) +
+            foo("c", toUpperCase = true) +
+            foo(name = "d", number = 2, toUpperCase = true))
 }
