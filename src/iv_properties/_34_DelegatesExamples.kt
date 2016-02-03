66255dd874a7f10f0feaaa65c77d90daf6b0b3b9
Michał Piekarski <michal.piekarski@byoutline.com>
2016-02-03 13:01:18 +0100

    delegates examples

--- src/iv_properties/_34_DelegatesExamples.kt
+++ src/iv_properties/_34_DelegatesExamples.kt
@@ -1,9 +1,10 @@
 package iv_properties
 
-import util.*
+import util.TODO
+import util.doc34
 
 class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by todoTask34()
+    val lazyValue: Int by lazy(initializer)
 }
 
 fun todoTask34(): Lazy<Int> = TODO(
