6f61e4a2b6da2f379c643e0f9bbe2c848838aa99
Michał Piekarski <michal.piekarski@byoutline.com>
2016-02-03 14:44:46 +0100

    html builders

--- src/v_builders/_39_HtmlBuilders.kt
+++ src/v_builders/_39_HtmlBuilders.kt
@@ -22,7 +22,7 @@ fun todoTask39(): Nothing = TODO(
 fun renderProductTable(): String {
     return html {
         table {
-            tr {
+            tr(color = getTitleColor()) {
                 td {
                     text("Product")
                 }
